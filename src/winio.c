e32782c9f6e0bc08ff2654dc65bc29a8cc41490b
Liu Hao <lh_mouse@126.com>
2018-02-06 16:07:06 +0800

    New patch for mingw targets.
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- src/winio.c
+++ src/winio.c
@@ -30,6 +30,7 @@
 #ifdef ENABLE_UTF8
 #include <wchar.h>
 #endif
+#include <windows.h>
 
 #ifdef REVISION
 #define BRANDING  REVISION
@@ -264,10 +265,13 @@ void read_keys_from(WINDOW *frame)
 	if (!key_buffer)
 		reserve_space_for(capacity);
 
-	key_buffer[0] = input;
-
 	nextcodes = key_buffer;
-	waiting_codes = 1;
+	waiting_codes = 0;
+
+	if (GetAsyncKeyState(VK_MENU) < 0)
+		key_buffer[waiting_codes++] = ESC_CODE;
+
+	key_buffer[waiting_codes++] = input;
 
 #ifndef NANO_TINY
 	/* Cancel the highlighting of a search match, if there still is one. */
@@ -303,6 +307,9 @@ void read_keys_from(WINDOW *frame)
 		/* When the keystroke buffer is full, extend it. */
 		if (waiting_codes == capacity)
 			reserve_space_for(2 * capacity);
+
+		if (GetAsyncKeyState(VK_MENU) < 0)
+			key_buffer[waiting_codes++] = ESC_CODE;
 
 		key_buffer[waiting_codes++] = input;
 	}
@@ -1180,13 +1187,18 @@ int parse_kbinput(WINDOW *frame)
 		return FOREIGN_SEQUENCE;
 #endif
 
-#ifdef __linux__
 	/* When not running under X, check for the bare arrow keys whether
 	 * Shift/Ctrl/Alt are being held together with them. */
-	unsigned char modifiers = 6;
+	unsigned char modifiers = 0;
 
 	/* Modifiers are: Alt (8), Ctrl (4), Shift (1). */
-	if (on_a_vt && !mute_modifiers && ioctl(0, TIOCLINUX, &modifiers) >= 0) {
+	if(GetAsyncKeyState(VK_SHIFT) < 0)
+		modifiers |= 0x01;
+	if(GetAsyncKeyState(VK_CONTROL) < 0)
+		modifiers |= 0x04;
+	if(GetAsyncKeyState(VK_MENU) < 0)
+		modifiers |= 0x08;
+	if (!mute_modifiers) {
 #ifndef NANO_TINY
 		/* Is Shift being held? */
 		if (modifiers & 0x01) {
@@ -1235,7 +1247,6 @@ int parse_kbinput(WINDOW *frame)
 		}
 #endif
 	}
-#endif /* __linux__ */
 
 #ifndef NANO_TINY
 	/* When <Tab> is pressed while the mark is on, do an indent. */
@@ -1506,12 +1517,6 @@ char *get_verbatim_kbinput(WINDOW *frame, size_t *count)
 	if (!ISSET(RAW_SEQUENCES))
 		keypad(frame, FALSE);
 
-#ifndef NANO_TINY
-	/* Turn bracketed-paste mode off. */
-	printf("\x1B[?2004l");
-	fflush(stdout);
-#endif
-
 	linger_after_escape = TRUE;
 
 	/* Read in a single byte or two escapes. */
@@ -1528,12 +1533,6 @@ char *get_verbatim_kbinput(WINDOW *frame, size_t *count)
 	}
 
 	linger_after_escape = FALSE;
-
-#ifndef NANO_TINY
-	/* Turn bracketed-paste mode back on. */
-	printf("\x1B[?2004h");
-	fflush(stdout);
-#endif
 
 	/* Turn flow control characters back on if necessary and turn the
 	 * keypad back on if necessary now that we're done. */
